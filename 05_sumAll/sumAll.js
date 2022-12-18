ff818b5d90d54f396a16075b531d864d3a7eef1e
kylr <107581523+khyllar@users.noreply.github.com>
2022-12-18 16:38:28 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0) return "ERROR";
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+    let sum = 0;
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+    return sum
 };
-
+sumAll(1, 3);
+console.log(sumAll(1, 3));
 // Do not edit below this line
 module.exports = sumAll;
