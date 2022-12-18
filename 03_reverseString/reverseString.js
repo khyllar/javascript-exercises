60bd3f4d4b3104dd949c86c2819d3050f400a069
kylr <107581523+khyllar@users.noreply.github.com>
2022-12-18 11:16:39 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let split = string.split('');
+    let reverse = split.reverse();
+    let join = reverse.join('');
+    return join;
 };
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
