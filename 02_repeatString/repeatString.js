7a6cab582a024af69941a251810867ea351ce4d2
kylr <107581523+khyllar@users.noreply.github.com>
2022-12-18 10:45:38 -0500

    Complete exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let word = '';
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
+    return word
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
