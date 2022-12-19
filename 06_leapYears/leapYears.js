a6143cff4a1cb1941e5d1e5f6faf03d2f8dd677e
kylr <107581523+khyllar@users.noreply.github.com>
2022-12-19 09:40:50 -0500

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 === 0 &&  year % 100 === 0) {
+        return true
+    } else if (year % 100 === 0) {
+        return false
+    } else if (year % 4 === 0) {
+        return true
+    } else {
+        return false
+    }
 
 };
 
+leapYears(2000); // is a leap year: returns true
+leapYears(1985); // is not a leap year: returns false
 // Do not edit below this line
 module.exports = leapYears;
